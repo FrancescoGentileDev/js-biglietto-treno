c9af96c81b8f1d3ead91cb473937d5987193fc39
svalvastic <assogames2@gmail.com>
2022-08-23 15:26:43 +0200

    ✨ feat(control): input control

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,20 @@
-const percorrenza = prompt("inserire il numero dei km da percorrere");
-const eta = prompt("inserire l'età del passeggero");
+let percorrenza = NaN
+let eta = NaN
+let costoTotale; 
 
-let costoTotale = percorrenza * 0.21;
+    while (isNaN(percorrenza) || isNaN(eta)) {
+        percorrenza = parseInt(prompt("inserire il numero dei km da percorrere"));
+        eta = parseInt( prompt("inserire l'età del passeggero"));
+    }
+   costoTotale = percorrenza * 0.21;
 
 if (eta < 18) {
-    costoTotale *= 0.2;
+    costoTotale -= costoTotale * 0.2;
 }
 else if (eta > 65) {
-    costoTotale *= 0.4
+    ccostoTotale -= costoTotale * 0.4;
 }
 
 costoTotale = costoTotale.toFixed(2)
 
-alert(`Il prezzo da pagare è ${costoTotale}`)
+alert(`Il prezzo da pagare è €${costoTotale}`)
